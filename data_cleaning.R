7a01b04e3300b992d81705e1dfa186ef38430a6d
csoderberg <csoderbe@gmail.com>
2020-03-16 13:47:07 -0400

    create deidentified dataset

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,10 +1,41 @@
 ##loading libraries
 library(osfr)
 library(tidyverse)
+library(here)
 
 
-#read in data
+#### Creating deidentified raw data set ####
 
+#download raw data
+osf_retrieve_file("https://osf.io/wjkg6/") %>%
+  osf_download()
+
+#read in raw data
+raw_numeric <- read_csv(here::here('/ERR2_rawdata_numeric.csv'))
+
+#how many will be excluded?
+raw_numeric %>%
+  slice(-1:-2) %>% #take out first 2 rows added by qualtrics
+  filter(FirstFamiliar == '1' | SecondFamiliar == '1') %>%
+  nrow()
+  
+# exclude anyone who got one of their own papers (in line with pre-reg)
+raw_deidentified_numeric <- raw_numeric %>%
+                                slice(-1:-2) %>% #take out first 2 rows added by qualtrics
+                                filter(FirstFamiliar != '1' & SecondFamiliar != '1')
+
+# created and upload deidentified raw data
+write_csv(raw_deidentified_numeric, 'deidentified_raw_numeric.csv')
+osf_upload('https://osf.io/q6pef/', oath = 'deidentified_raw_numeric.csv')
+
+
+#### Creating Cleaned data file ####
+
+#download & read in deidentifed raw data
+osf_retrieve_file("") %>%
+  osf_download()
+
+deidentified_raw_numeric <- read_csv(here::here('/deidentified_raw_numeric.csv'))
 
 
 #create difference scores
