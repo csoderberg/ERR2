4f73612d98bf6fae78da0545b610b2d726324a82
csoderberg <csoderbe@gmail.com>
2020-05-31 18:45:02 -0400

    create supplements for each btw_subj variable

--- create_supplements.R
+++ create_supplements.R
@@ -15,4 +15,22 @@ supplements <- tibble(
 supplements %>%
   pwalk(rmarkdown::render, 'dv_supplements.Rmd')
 
-  
+
+# supplements by variable for between data
+long_data <- read_csv(here::here('cleaned_numeric_data_long.csv'), col_types = cols(article_type = col_factor(),
+                                                                                    Field = col_factor(),
+                                                                                    Match = col_factor(),
+                                                                                    Order = col_factor(),
+                                                                                    keyword_batch_comp = col_factor())) %>%
+  mutate(question = case_when(article_type == 'RR' ~ str_sub(question, 3),
+                              article_type == 'nonRR' ~ str_sub(question, 4)))
+
+btw_dep_var <- long_data %>% select(question) %>% distinct(question)
+
+btw_supplements <- tibble(
+  output_file = stringr::str_c(btw_var, "btwsubj_supplement.html"),
+  params = map(btw_dep_var, ~list(btw_dep_var = .))
+)
+
+supplements %>%
+  pwalk(rmarkdown::render, 'dv_supplements_between.Rmd')
