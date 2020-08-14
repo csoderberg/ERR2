45839b782034d438bec0c5aab4a64b81d0874d27
csoderberg <csoderbe@gmail.com>
2020-08-14 16:00:15 -0400

    calculate btw covariates and add model for them

--- analysis_script.R
+++ analysis_script.R
@@ -7,6 +7,7 @@ library(tidybayes)
 library(patchwork)
 library(broom)
 library(dotwhisker)
+library(skimr)
 
 # load data
 long_data <- read_csv(here::here('cleaned_numeric_data_long.csv'), col_types = cols(article_type = col_factor(),
@@ -16,7 +17,14 @@ long_data <- read_csv(here::here('cleaned_numeric_data_long.csv'), col_types = c
                                                                                     keyword_batch_comp = col_factor())) %>%
              mutate(question = case_when(article_type == 'RR' ~ str_sub(question, 3),
                                          article_type == 'nonRR' ~ str_sub(question, 4))) %>%
-            mutate(article_type = fct_relevel(article_type, c('nonRR', 'RR')))
+            mutate(article_type = fct_relevel(article_type, c('nonRR', 'RR'))) %>%
+            mutate(guessed_right = case_when(Order == 'RRFirst' & BelieveFirstRR == 1 ~ 1,
+                                             Order == 'RRFirst' & (BelieveFirstRR == 2 | BelieveFirstRR == 3) ~ 0,
+                                             Order == 'RRSecond' & BelieveFirstRR == 1 ~ 0,
+                                             Order == 'RRSecond' & (BelieveFirstRR == 2 | BelieveFirstRR == 3) ~ 1)) %>%
+            mutate(behavior_familiar = rowMeans(across(c(RRFamiliar,PreregFamiliar)), na.rm = T),
+                   believe_improve = rowMeans(across(c(BelieveRigor,BelieveQuality)), na.rm = T))
+
 wide_data <- read_csv(here::here('cleaned_numeric_data_wide.csv'), col_types = cols(Field = col_factor(),
                                                                                     Match = col_factor(),
                                                                                     Order = col_factor(),
@@ -46,7 +54,7 @@ priors <- c(set_prior("normal(0,2)", "b"),
 
 
 
-### functions for within models
+#### functions for within models ####
 
 # set up function to run within difference pooled models on all dvs
 within_diff_pooled_model <- function(dv, set_priors) {
@@ -114,7 +122,7 @@ mcmc_intervals(intercepts, prob = .95)
 
 
 
-### functions for between subjects models
+#### functions for between subjects models ####
 
 # between-subjects models comparing pooled, batch 1, batch 2 + 3
 between_pooled_model <- function(dv, set_priors) {
@@ -158,8 +166,8 @@ between_keywords2_model <- function(dv, set_priors) {
 # function to create graph of posterior samples for intercept
 create_posteriors_btw <- function(results, variable){
   posteriors <- list(posterior_samples(results) %>%
-                       select('b_article_type1') %>%
-                       rename(setNames('b_article_type1', variable)))
+                       select('b_article_type2') %>%
+                       rename(setNames('b_article_type2', variable)))
   
   return(posteriors)
 }
@@ -217,3 +225,161 @@ bind_rows(tidy(between_model) %>% mutate(model = 'between_model'),
   dotwhisker::dwplot() 
 
 between_posteriors_keywords1 / between_posteriors_keywords2
+
+
+#### demographics and sample descriptives ####
+for_descriptive <- wide_data %>%
+  select(-starts_with('diff'), -starts_with('Alt'), -c(StartDate, EndDate)) %>%
+  select(Field, SPSubfield, Keyword, Gender, Education, ProfTitle, FirstFamiliar, FirstQualified,
+         SecondFamiliar, SecondQualified, RRFamiliar, EverRR, PreregFamiliar, EverPrereg,
+         BelieveRigor, BelieveQuality, BelieveFirstRR, BelieveSecondRR, RR, Order, Match)
+
+
+
+## Overall descriptives
+for_descriptive %>%
+  select(-c(Field, SPSubfield, Keyword, Gender, Education, ProfTitle, BelieveFirstRR, BelieveSecondRR, RR, Order, Match, EverRR, EverPrereg), -starts_with('First'), -starts_with('Second')) %>%
+  skim_to_wide() %>%
+  mutate(missing = as.numeric(missing),
+         complete = as.numeric(complete),
+         n = as.numeric(n),
+         mean = as.numeric(mean),
+         sd = as.numeric(mean),
+         p0 = as.numeric(p0),
+         p25 = as.numeric(p25),
+         p50 = as.numeric(p50),
+         p75 = as.numeric(p75),
+         p100 = as.numeric(p100)) %>%
+  gt()
+
+for_descriptive %>%
+  select(RRFamiliar) %>%
+  group_by(RRFamiliar) %>%
+  tally() %>%
+  mutate(RRFamiliar = as.factor(RRFamiliar),
+         RRFamiliar = fct_recode(RRFamiliar, `Not at all familiar` = "1", `Slightly familiary` = "2", `Somewhat familiary` = "3",
+                                     `Moderately familiary` = "4", `Substantially familiar` = "5"))
+
+for_descriptive %>%
+  select(PreregFamiliar) %>%
+  group_by(PreregFamiliar) %>%
+  tally() %>%
+  mutate(PreregFamiliar = as.factor(PreregFamiliar),
+         PreregFamiliar = fct_recode(PreregFamiliar, `Not at all familiar` = "1", `Slightly familiary` = "2", `Somewhat familiary` = "3",
+                                 `Moderately familiary` = "4", `Substantially familiar` = "5"))
+
+for_descriptive %>%
+  select(BelieveRigor) %>%
+  group_by(BelieveRigor) %>%
+  tally() %>%
+  mutate(BelieveRigor = as.factor(BelieveRigor),
+         BelieveRigor = fct_recode(BelieveRigor,
+                                   `Subtantially less` = "-4", `Much less` = "-3", `Moderately less` = "-2", `Slightly less` = "-1", `Not change` = "0",
+                                   `Subtantially more` = "4", `Much more` = "3", `Moderately more` = "2", `Slightly more` = "1" ))
+
+for_descriptive %>%
+  select(BelieveQuality) %>%
+  group_by(BelieveQuality) %>%
+  tally() %>%
+  mutate(BelieveQuality = as.factor(BelieveQuality),
+         BelieveQuality = fct_recode(BelieveQuality,
+                                   `Subtantially less` = "-4", `Much less` = "-3", `Moderately less` = "-2", `Slightly less` = "-1", `Not change` = "0",
+                                   `Subtantially more` = "4", `Much more` = "3", `Moderately more` = "2", `Slightly more` = "1" ))
+
+
+#### RR guesses
+
+## RR guesses by order
+for_descriptive %>%
+  mutate(RR_isRR = case_when(Order == 'RRFirst' ~ BelieveFirstRR,
+                                  Order == 'RRSecond' ~ BelieveSecondRR),
+         Alt_isRR = case_when(Order == 'RRFirst' ~ BelieveSecondRR,
+                                   Order == 'RRSecond' ~ BelieveFirstRR)) %>%
+  summarize(RR_yesRR = sum(RR_isRR == 1), RR_notRR = sum(RR_isRR == 3), RR_unsure = sum(RR_isRR == 2),
+            Alt_yesRR = sum(Alt_isRR == 1), Alt_notRR = sum(Alt_isRR == 3), Alt_unsure = sum(Alt_isRR == 2))
+
+# RR guesses by article & order
+for_descriptive %>%
+  mutate(RR_isRR = case_when(Order == 'RRFirst' ~ BelieveFirstRR,
+                             Order == 'RRSecond' ~ BelieveSecondRR),
+         Alt_isRR = case_when(Order == 'RRFirst' ~ BelieveSecondRR,
+                              Order == 'RRSecond' ~ BelieveFirstRR)) %>%
+  group_by(RR) %>%
+  summarize(RR_yesRR = sum(RR_isRR == 1), RR_notRR = sum(RR_isRR == 3), RR_unsure = sum(RR_isRR == 2),
+            Alt_yesRR = sum(Alt_isRR == 1), Alt_notRR = sum(Alt_isRR == 3), Alt_unsure = sum(Alt_isRR == 2))
+
+#### Article review qualifications
+
+# by order
+for_descriptive %>%
+  mutate(RR_qualified = case_when(Order == 'RRFirst' ~ FirstQualified,
+                                  Order == 'RRSecond' ~ SecondQualified),
+         Alt_qualified = case_when(Order == 'RRFirst' ~ SecondQualified,
+                                   Order == 'RRSecond' ~ FirstQualified)) %>%
+  select(Order, RR_qualified, Alt_qualified) %>%
+  summarize(RR_qualified_mean = mean(RR_qualified), RR_qualified_sd = sd(RR_qualified),
+            Alt_qualified_mean = mean(Alt_qualified), Alt_qualified_sd = sd(Alt_qualified))
+
+# by order & article
+for_descriptive %>%
+  mutate(RR_qualified = case_when(Order == 'RRFirst' ~ FirstQualified,
+                                  Order == 'RRSecond' ~ SecondQualified),
+         Alt_qualified = case_when(Order == 'RRFirst' ~ SecondQualified,
+                                   Order == 'RRSecond' ~ FirstQualified)) %>%
+  select(RR, RR_qualified, Alt_qualified) %>%
+  group_by(RR) %>%
+  summarize(RR_qualified_mean = mean(RR_qualified), RR_qualified_sd = sd(RR_qualified),
+            Alt_qualified_mean = mean(Alt_qualified), Alt_qualified_sd = sd(Alt_qualified))
+
+#### RR/Prereg behavior
+
+for_descriptive %>%
+  group_by(EverPrereg) %>%
+  tally() %>%
+  mutate(EverPrereg = as.factor(EverPrereg),
+         EverPrereg = fct_recode(EverPrereg,
+                                 No = '1', `Yes, once` = '2', `Yes, a few times` = '3', `Yes, many times` = '4'))
+  
+for_descriptive %>%
+  group_by(EverRR) %>%
+  tally() %>%
+  mutate(EverRR = as.factor(EverRR),
+         EverRR = fct_recode(EverRR,
+                                 No = '2', Yes = '1'))
+
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+
+for_descriptive %>%
+  select(BelieveQuality, BelieveRigor, PreregFamiliar, RRFamiliar, FirstQualified, SecondQualified, Order) %>%
+  mutate(RR_qualified = case_when(Order == 'RRFirst' ~ FirstQualified,
+                                  Order == 'RRSecond' ~ SecondQualified),
+         Alt_qualified = case_when(Order == 'RRFirst' ~ SecondQualified,
+                                   Order == 'RRSecond' ~ FirstQualified)) %>%
+  select(-c("Order", "FirstQualified", "SecondQualified")) %>%
+  cor(use = 'everything') %>%
+  corrplot(method="color", col=col(200),  
+           type="lower", order="hclust", 
+           addCoef.col = "black", # Add coefficient of correlation
+           tl.col="black", tl.srt=45, #Text label color and rotation
+           # hide correlation coefficient on the principal diagonal
+           diag=FALSE 
+  )
+  
+
+#### Exploratory Analyses ####
+
+## add covariates for 'Qualified to review', behavior_familiar (item average score of Pre-reg adn RR familiarity items), believe_improve (item average score of BelieveRigor and BelieveQuality items)
+  
+between_pooled_model_covariates <- function(dv, set_priors) {
+  between_model <- brm(response ~ Field + keyword_batch_comp + FirstQualified + behavior_familiar + believe_improve
+                                      + article_type + Match + article_type*Match +
+                         (article_type|RR),
+                       data = long_data %>% filter(grepl(as.character(dv), question)) %>% filter((Order == 'RRFirst' & article_type == 'RR') | (Order == 'RRSecond' & article_type == 'nonRR')),
+                       prior = priors,
+                       family = 'gaussian',
+                       chains = 4)
+  return(between_model)
+}
+  
+  
+  
