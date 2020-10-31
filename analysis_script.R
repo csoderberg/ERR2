c8720a2f546a070833e1f3cdac63b9ce12f14fae
csoderberg <csoderbe@gmail.com>
2020-10-31 16:23:31 -0400

    get btw model posteriors averaging across RR pairs

--- analysis_script.R
+++ analysis_script.R
@@ -565,12 +565,13 @@ between_model_mlm_slopes <- brm(response ~ Field + keyword_batch_comp + article_
 
 # get posteriors by DV
 posteriors_btw_mlm <- between_model_mlm_slopes %>%
-                          spread_draws(b_article_type2, r_RR[,article_type], r_question[DV, article_type]) %>%
+                          spread_draws(b_article_type2, r_question[DV, article_type]) %>%
                           filter(article_type == 'article_type2') %>%
-                          mean_qi(article_effect = b_article_type2 + r_RR + r_question, .width = c(.95, .80))
+                          mean_qi(article_effect = b_article_type2 + r_question, .width = c(.95, .80))
 
 # get overall, min and max across DVs
 posteriors_btw_mlm %>%
+  ungroup() %>%
   filter(.width == 0.95) %>%
   summarize(mean = round(mean(article_effect),2), min = round(min(article_effect),2), max = round(max(article_effect),2))
 
